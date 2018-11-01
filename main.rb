864af671fed7e28b0865f29f12e408cbbba562c4
TarrinRos <tarindis@gmail.com>
2018-11-01 22:48:11 +0200

    Add ability to choose town for forecast

--- main.rb
+++ main.rb
@@ -11,8 +11,28 @@ if (Gem.win_platform?)
   end
 end
 
-uri = URI.parse('https://xml.meteoservice.ru/export/gismeteo/point/312.xml')
+TOWNS = {'Москва': '37', 'Рига': '312', 'ГонКонг': '256', 'Женева': '374', 'Вашингтон': '384', 'Каир': '334'}
 
+# Получает все названия городов, формирует из них массив, сортирует по алфавиту
+towns_list = TOWNS.keys.to_a.sort
+
+puts
+puts 'В каком городе Вы хотели бы посмотреть прогноз погоды?'
+puts
+
+# выводт список на экран с индексом
+towns_list.each_with_index do |town, index|
+  puts "#{index + 1}. #{town}"
+end
+
+user_choice = STDIN.gets.to_i
+user_choice -= 1
+choiced_town = towns_list[user_choice]
+
+# Формирует ссылку на XML файл исходя из выбранного города и отправляет запрос
+uri = URI.parse("https://xml.meteoservice.ru/export/gismeteo/point/#{TOWNS[choiced_town]}.xml")
+
+# Получает ответ от сервера по указаной ссылке
 response = Net::HTTP.get_response(uri)
 
 collector = MeteoCollector.get_data_from_xml(response)
